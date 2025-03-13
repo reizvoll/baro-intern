cf5e190c05bad749293a959e7a5b6adbc577629e
reizvoll <mara2277@naver.com>
2025-03-13 21:02:42 +0900

    feat: tanstack-query를 활용한 Provider 생성, layout.tsx에 감싸주기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "@/styles/globals.css";
+import TQProviders from "@/providers/TQProvider";
 
 const pretendard = localFont({
   src: "../assets/fonts/PretendardVariable.woff2",
@@ -21,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={pretendard.className}>{children}</body>
+      <body className={pretendard.className}>
+        <TQProviders>{children}</TQProviders>
+      </body>
     </html>
   );
 }
