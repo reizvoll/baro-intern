7827ccf7bd3d6e95777d4f4a1ad0ec29db2c16d3
reizvoll <mara2277@naver.com>
2025-03-15 17:34:50 +0900

    fix: 속도 최적화를 위한 SSR 도입

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import TodoForm from "@/components/TodoForm";
 import TodoList from "@/components/TodoList";
+import { fetchTodos } from "@/api/api";
+import { Todo } from "@/types/todo";
 
-export default function Home() {
+export default async function Home() {
+  const todos: Todo[] = await fetchTodos();
+
   return (
     <main className="mx-auto max-w-[1200px] px-4 pt-10 tb:px-6">
       <h1 className="text-title1 mb:text-title2 text-center font-bold">TODO 리스트</h1>
