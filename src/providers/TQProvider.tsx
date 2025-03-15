1b7ce30e822008426dcffcb462e3ac5eb58e76ff
reizvoll <mara2277@naver.com>
2025-03-15 18:05:53 +0900

    fix: 불필요한 기능 제거

--- src/providers/TQProvider.tsx
+++ src/providers/TQProvider.tsx
@@ -1,35 +1,18 @@
 "use client";
 
-import { isServer, QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
 
-function makeQueryClient() {
-  return new QueryClient({
+export default function TQProviders({ children }: { children: ReactNode }) {
+  // useState를 사용해 클라이언트에서 단일 QueryClient 유지
+  const [queryClient] = useState(() => new QueryClient({
     defaultOptions: {
       queries: {
         staleTime: 60 * 1000, // 데이터 갱신 빈도 제어
-        refetchOnWindowFocus: false // 불팔요한 API 호출 감소
+        refetchOnWindowFocus: false // 불필요한 API 호출 방지
       }
     }
-  });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-// SSR 환경, 요청마다 새로운 QueryClient 생성 -> 데이터 격리 보장
-// 클라이언트 환경, 단일 인스턴스를 재사용 -> 캐싱의 효율성을 향상
-
-function getQueryClient() {
-  if (isServer) {
-    return makeQueryClient();
-  } else {
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
-
-export default function TQProviders({ children }: { children: ReactNode }) {
-  const queryClient = getQueryClient();
+  }));
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
