39aa47b8c0a6d65b2f5514c564ff6e9163c59d7e
reizvoll <mara2277@naver.com>
2025-03-15 13:49:27 +0900

    style: 커스텀 폰트 조건 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,13 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
+    fontSize: {
+      title1: ["24px", { lineHeight: "150%", letterSpacing: "-0.025em" }],
+      title2: ["18px", { lineHeight: "150%", letterSpacing: "-0.015em" }],
+      body1: ["16px", { lineHeight: "150%", letterSpacing: "0" }],
+      body2: ["14px", { lineHeight: "150%", letterSpacing: "0" }],
+      body3: ["12px", { lineHeight: "150%", letterSpacing: "0" }]
+    },
     extend: {
       screens: {
         tb: { max: "768px" }, // 일반 타블렛 크기
